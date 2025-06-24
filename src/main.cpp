221ab6eeb40e92bc8b0359732a87dd2848628d2a
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2025-06-25 00:07:59 +0800

    feat: use boost from bazel (#17)

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,11 @@
 #include <iostream>
 
 #include "app.h"
+#include "boost/asio/io_context.hpp"
 
-int
-main()
+int main()
 {
+  boost::asio::io_context io_context;
   App app;
   app.build();
   return 0;
