1a97ef983205394efa0f08747e954d49661121ea
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2025-08-03 15:55:29 +0800

    feat: read arguments from command line (#23)

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,31 @@
 #include <iostream>
 
 #include "app.h"
-#include "boost/asio/io_context.hpp"
+#include <boost/program_options.hpp>
 
-int main()
-{
-  boost::asio::io_context io_context;
+int main(int argc, char* argv[]) {
+  namespace po = boost::program_options;
+  po::options_description desc("Allowed options");
+  desc.add_options()
+      ("help,h", "produce help message");
+
+  po::variables_map vm;
+  try {
+      po::store(po::parse_command_line(argc, argv, desc), vm);
+
+      if (vm.count("help")) {
+          std::cout << desc << "\n";
+          return 0;
+      }
+
+      po::notify(vm);
+
+  } catch (std::exception& e) {
+      std::cerr << "Error: " << e.what() << "\n";
+      std::cerr << desc << "\n";
+      return 1;
+  }
+
   App app;
   app.build();
   return 0;
